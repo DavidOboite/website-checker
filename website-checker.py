a2fd5d01e1d92fcc026ef39afec4633abe1b156e
David Oboite <eromoobo@gmail.com>
2026-04-24 18:01:44 +0000

    Debugging

--- website-checker.py
+++ website-checker.py
@@ -1,7 +1,7 @@
 import sys
-from ssl import SSLError
-
 import requests
+from requests.exceptions import SSLError, ConnectionError, Timeout, RequestException
+
 if __name__ == '__main__':
     try:
         print(f'Reading URLs from {sys.argv[1]!r}')
@@ -10,17 +10,25 @@ if __name__ == '__main__':
             for possible_url in f:
                 try:
                     possible_url = possible_url.strip()
-                    r  = requests.get(possible_url)
-                    if r.status_code !=  200:
-                        print(f'{possible_url!r} Responded, but resource not available')
+                    r = requests.get(possible_url, timeout=5)
+
+                    if r.status_code != 200:
+                        print(f'{possible_url!r} responded, but resource not available ({r.status_code})')
+
                 except SSLError:
                     print(f'{possible_url!r} SSL error')
-                except ConnectionError:
-                    print(f'{possible_url!r} Connection error')
-                except:
-                    print(f'Something unexpected happened with {possible_url!r}.')
-    except IndexError:
-        print('Missing URL')
-    except  FileNotFoundError:
-        print(f'Cannot open {sys.argv[1]!r}')
 
+                except ConnectionError:
+                    print(f'{possible_url!r} connection error')
+
+                except Timeout:
+                    print(f'{possible_url!r} timeout')
+
+                except RequestException as e:
+                    print(f'{possible_url!r} request error: {e}')
+
+    except IndexError:
+        print('Missing URL file argument')
+
+    except FileNotFoundError:
+        print(f'Cannot open {sys.argv[1]!r}')
